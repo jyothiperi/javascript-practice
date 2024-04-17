e803fab9730d3d5090ec79ba776d5620e1701661
Sree Peri <jyothi_sarma@yahoo.com>
2024-04-17 10:51:42 +0200

    files added

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+
+// take the operand input
+var number1 = 5;
+var number2 = 6;
+var operator = "/";
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
---
+++ multipicationtable.js
@@ -0,0 +1,12 @@
+// To create a multiplication table
+var number = 5;
+// creating the multiplication tabel
+
+for (let i = 1; i < 10; i++){
+
+    // to multiple i with number
+    var result = i * number;
+
+// To display result
+console.log(result);
+}
